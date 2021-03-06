063bb93e530a24a2d346a8d17b64215268e2928e
HRodenhizer <heidi.rodenhizer@yahoo.com>
2021-03-05 17:39:02 -0700

    begin implementing flux footprint prediction at EML

--- flux_footprint_estimation/flux_footprint_tool.R
+++ flux_footprint_estimation/flux_footprint_tool.R
@@ -1,11 +1,13 @@
 #########################################################################################
-###                         Format Flux Data for FFP Online Tool                      ###
+###                          Run FFP on EML Tower 2017-2019                        ###
 ###                                Code by HGR 2/2021                                 ###
 #########################################################################################
 
 ### Libraries ###########################################################################
+library(sf)
 library(data.table)
 library(lubridate)
+library(viridis)
 library(tidyverse)
 #########################################################################################
 
@@ -63,8 +65,9 @@ flux.format[is.na(u_star), u_star := -999]
 flux.format[is.na(wind_dir), wind_dir := -999]
 flux.format[is.na(L), L := -999]
 flux.format[is.na(sigma_v), sigma_v := -999]
-flux.format[, zm := 3.5]
+flux.format[, z := 3.5]
 flux.format[, d := 0.2]
+flux.format[, zm := z - d]
 flux.format[, z0 := 0.05]
 
 ### plot to make sure everything looks good
@@ -122,3 +125,69 @@ final.19 <- final[yyyy == 2019 & mm >= 5 | yyyy == 2020 & mm < 5]
 # write.csv(final.17,
 #           '/home/heidi/Documents/School/NAU/Schuur Lab/Remote Sensing/thermokarst_project/flux_tower_footprint/ffp_ready_2017.csv',
 #           row.names = FALSE)
+#########################################################################################
+
+### Run FFP #############################################################################
+# To use this function, you have to run the function in calc_footprint_FFP.R
+test.data <- final.17[4300,]
+latitude <- 63.879933
+longitude <- -149.252103
+test <- calc_footprint_FFP(lat = latitude,
+                           zm = as.numeric(test.data[,"zm"]),
+                           z0 = as.numeric(test.data[,"z0"]),
+                           #h = 1000,
+                           umean = as.numeric(test.data[,"u_mean"]),
+                           ol = as.numeric(test.data[, "L"]),
+                           sigmav = as.numeric(test.data[, "sigma_v"]),
+                           ustar = as.numeric(test.data[, "u_star"]),
+                           wind_dir = as.numeric(test.data[, "wind_dir"]),
+                           r = seq(10, 90, 10))
+
+# format as data frame to plot with ggplot
+x <- test$x_2d %>%
+  as.data.frame() %>%
+  pivot_longer(cols = 1:1001, names_to = 'remove', values_to = 'x') %>%
+  select(x) %>%
+  data.table()
+y <- test$y_2d %>%
+  as.data.frame() %>%
+  pivot_longer(cols = 1:1001, names_to = 'remove', values_to = 'y') %>%
+  select(y) %>%
+  data.table()
+f <- test$f_2d %>%
+  as.data.frame() %>%
+  pivot_longer(cols = 1:1001, names_to = 'remove', values_to = 'f') %>%
+  select(f) %>%
+  data.table()
+test.df <- cbind(x, y, f)
+
+# reformat contours
+xr <- test$xr[[1]]
+yr <- test$yr[[1]]
+matrix.10 <- matrix(c(xr, yr), ncol = 2, byrow = FALSE)
+sfc.10 <- st_sfc(st_polygon(list(matrix.10)))
+sf.10 <- st_sf(geometry = sfc.10)
+
+r.sfc <- vector()
+for (i in 1:length(test$xr)) {
+  xr <- test$xr[[i]]
+  yr <- test$yr[[i]]
+  r.polygon[i] <- st_polygon(list(matrix(c(xr, yr), ncol = 2, byrow = FALSE)))
+}
+r.sfc <- st_as_sfc(r.polygon)
+r.sf <- st_sf(geometry = test.sfc)
+
+# sum of all weights - why not 1?
+# sum changes for each time period, so I think it just needs to be scaled?
+sum(test.df$f)
+
+# plot
+ggplot(filter(test.df, x < 0 & x > -275 & y < 100 & y > -50), aes(x = x, y = y, color = sqrt(f))) +
+  geom_point() +
+  # coord_fixed() +
+  geom_sf(data = r.sf, inherit.aes = FALSE, color = 'red', fill = 'transparent') +
+  scale_color_viridis()
+
+ggplot(test.df, aes(x = f)) +
+  geom_bar()
+#########################################################################################
